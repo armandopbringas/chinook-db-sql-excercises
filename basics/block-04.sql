0a392db7862a6fc0320e4b8515906e9195c76143
Armando Bringas <bringas.armandop@gmail.com>
2025-12-08 18:06:49 -0600

    Finish queries for block 5

--- basics/block-04.sql
+++ basics/block-04.sql
@@ -1,20 +1,29 @@
--- Bloque 4 — DISTINCT + LIMIT
---
+/* ---------------------------------------------------------
+   Bloque 4 — DISTINCT + LIMIT
+----------------------------------------------------------*/
+
 -- 14. Países únicos
---
 -- Muestra todos los países únicos donde hay clientes (Customer.Country).
-SELECT DISTINCT CustomerId, Country FROM Customer;
--- ⸻
---
+SELECT DISTINCT Country
+FROM Customer
+ORDER BY Country ASC;
+
 -- 15. Primeras 5 pistas
---
 -- Devuelve las primeras cinco pistas de la tabla Track.
-SELECT TrackId, NAME FROM Track LIMIT 5;
---
+SELECT TrackId, Name
+FROM Track
+ORDER BY TrackId
+LIMIT 5;
+
 -- 16. Paginación
 -- Devuelve las pistas entre las posiciones 11 y 20 usando LIMIT y OFFSET.
-SELECT TrackId, NAME FROM Track ORDER BY TrackId LIMIT 10 OFFSET 10;
---
+SELECT TrackId, Name
+FROM Track
+ORDER BY TrackId
+LIMIT 10 OFFSET 10;
+
 -- 17. Géneros únicos ordenados
 -- Muestra los valores únicos de Genre.Name, ordenados alfabéticamente.
-SELECT Name FROM Genre ORDER BY Name ASC;
+SELECT DISTINCT Name
+FROM Genre
+ORDER BY Name ASC;
--- basics/block-05.sql
+++ basics/block-05.sql
@@ -3,27 +3,26 @@
 -- 18. Total de clientes
 --
 -- Cuenta cuántos clientes existen usando COUNT(*).
---
--- ⸻
---
+SELECT COUNT(*) FROM Customer;
 -- 19. Precio máximo y mínimo de pistas
 --
 -- Obtén el precio máximo (UnitPrice) y el precio mínimo de la tabla Track.
---
+SELECT
+    MAX(UnitPrice) AS MaxPrice,
+    MIN(UnitPrice) AS MinPrice
+FROM Track;
 -- ⸻
 --
 -- 20. Ventas totales
---
 -- Suma todos los montos de facturas (Invoice.Total).
---
+SELECT SUM(Total) AS TotalReveanue FROM Invoice;
 -- ⸻
 --
 -- 21. Duración promedio
---
 -- Calcula la duración promedio de todas las pistas (Milliseconds).
+SELECT AVG(Milliseconds) AS AvgDuration FROM Track;
 --
--- ⸻
 --
 -- 22. Promedio redondeado
---
 -- Calcula el precio promedio de las pistas (UnitPrice) redondeado a dos decimales.
+SELECT ROUND(AVG(UnitPrice), 2) AS AvgPriceTrack FROM Track;
