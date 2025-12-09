4e0b22384e8712bc29897d0dda980b9258e1f31b
Armando Bringas <bringas.armandop@gmail.com>
2025-12-08 20:35:42 -0600

    Format for block 1 queries file

--- basics/block-01.sql
+++ basics/block-01.sql
@@ -1,27 +1,45 @@
--- Bloque 1 — SELECT + comprensión del modelo
+/* ---------------------------------------------------------
+   Bloque 1 — SELECT básicos
+----------------------------------------------------------*/
 
+-- Modelo base
 SHOW TABLES;
+
+-----------------------------------------------------------
 
 -- 1. Listar clientes (Customer)
 -- Muestra los primeros 10 clientes, mostrando únicamente:
--- CustomerId, FirstName, LastName, Country.DESC Customer;
+-- CustomerId, FirstName, LastName, Country.
+SELECT 
+    CustomerId,
+    FirstName,
+    LastName,
+    Country
+FROM Customer
+LIMIT 10;
 
-SELECT * FROM Customer LIMIT 10;
+-----------------------------------------------------------
 
 -- 2. Listar artistas (Artist)
 -- Obtén la lista de todos los artistas, mostrando solo su ArtistId y Name.
+SELECT 
+    ArtistId,
+    Name
+FROM Artist;
 
-DESC Artist;
-SELECT * FROM Artist;
+-----------------------------------------------------------
 
 -- 3. Seleccionar columnas específicas
 -- Consulta la tabla Album y muestra solo:
 -- AlbumId y Title.
+SELECT 
+    AlbumId,
+    Title
+FROM Album;
 
-DESC Album;
-SELECT AlbumId, Title FROM Album;
+-----------------------------------------------------------
 
 -- 4. Contar cuántos registros tiene cada tabla base
---  Consulta la tabla Track y devuelve cuántas pistas existen en total usando COUNT().
-
-SELECT COUNT(*) FROM Track;
+-- Consulta la tabla Track y devuelve cuántas pistas existen en total usando COUNT().
+SELECT COUNT(*) AS TotalTracks
+FROM Track;
